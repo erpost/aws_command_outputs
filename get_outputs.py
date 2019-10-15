11213befd6ba6afd6f023ebb3dce990b6321ada1
Eric R Post <erpost@Erics-Office-MacOS-High-Sierra.local>
2019-10-15 12:39:49 -0400

    Initial commit

---
+++ get_outputs.py
@@ -0,0 +1,32 @@
+import requests
+import os
+from bs4 import BeautifulSoup
+
+
+urls = []
+path = os.path.expanduser('output-files/')
+
+if os.path.isdir(path):
+    pass
+else:
+    os.mkdir(path)
+
+with open('urls.txt') as f:
+    for line in f:
+        urls.append(line.rstrip('\n'))
+
+for url in urls:
+    print(url)
+    page = requests.get(url)
+
+    # Create BeautifulSoup object
+    soup = BeautifulSoup(page.text, 'html.parser')
+    result = soup.find("div", {"id": "output"})
+    # print(result)
+
+    outfile = url.split('/reference/')[1].replace('/', '-')
+    file_path = os.path.join(path, outfile)
+    print('File Created: ', outfile)
+    with open(file_path, 'w') as out_file:
+        for line in str(result):
+            out_file.write(line)
